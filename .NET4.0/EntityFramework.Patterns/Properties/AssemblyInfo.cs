6bb3c784c407c122ccb58bcebecee3fb70f58d7a
atachimiko <atachi.miko@gmail.com>
2013-10-21 21:11:46 +0900

    Release 1.1.0

--- .NET4.0/EntityFramework.Patterns/Properties/AssemblyInfo.cs
+++ .NET4.0/EntityFramework.Patterns/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using EntityFramework.Patterns;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyVersion("1.1.0.*")]
 [assembly: Amend]
