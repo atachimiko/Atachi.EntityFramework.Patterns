347ac7f794c9cc5380f34942d60d0d2147cff5bd
atachimiko <atachi.miko@gmail.com>
2013-12-17 02:27:49 +0900

    バージョン表記

--- EF5/.NET4.0/EntityFramework.Patterns/Properties/AssemblyInfo.cs
+++ EF5/.NET4.0/EntityFramework.Patterns/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("EntityFramework.Patterns")]
+[assembly: AssemblyTitle("EntityFramework.Patterns For EF5")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: Amend]
